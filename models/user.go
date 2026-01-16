ed2ed79c9d94fd3381df64780b70406421c8e782
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-01-16 12:41:36 +0300

    feat:function to create new user

--- models/user.go
+++ models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID				  string `bson:"_id,omitempty" json:"id"`
 	Email  			  string `bson:"email" json:"email"`
@@ -7,3 +9,16 @@ type User struct {
 	IsAdmin			  bool   `bson:"isAdmin" json:"isAdmin"`
 	Token			  string `bson:"token" json:"token"`
 }
+
+//creatring new user
+func NewUser(email, password string) (*User, error){
+	epw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		Email: 			   email,
+		EncryptedPassword: string(epw),
+	}, nil
+}
+
