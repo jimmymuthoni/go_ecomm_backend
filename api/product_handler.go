2ee1b8d2118908fc9fedc357c9b7ca84531ea839
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-01-16 17:55:22 +0300

    feat: handling post method for product

---
+++ api/product_handler.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/jimmymuthoni/weavebox"
+	"github.com/jimmymuthoni/go_ecomm_backend/models"
+	"github.com/jimmymuthoni/go_ecomm_backend/store"
+)
+
+type ProductHandler struct {
+	store 	store.ProductStorer
+}
+
+func NewProductHandler(pStore store.ProductStorer) *ProductHandler {
+	return &ProductHandler{
+		store: pStore,
+	}
+}
+
+func (h *ProductHandler) HandlePostProduct (c *weaviate.Context) error{
+	productReq := &models.CreateProductRequest{}
+	if err := json.NewDecoder(c.Request().Body).Decode(productReq); err != nil {
+		return err
+	}
+	product, err := models.NewProductFromRequest(productReq)
+	if err != nil {
+		return err
+	}
+
+	if err := h.store.Insert(c.Context, product); err != nil {
+		return err
+	}
+	return c.Json(http.StatusOK, product)
+}
